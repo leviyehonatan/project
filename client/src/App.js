aeb2d137144d5717a77ddb4f7c6c5be2556ac2a7
Yehonatan Levi <leviyehonatan@gmail.com>
2021-10-31 18:00:43 +0200

    added register call added cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+
+async function register(email, password) {
+  const response = await fetch('http://localhost:4000/register', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password })
+  }).catch(err => {
+    console.log(err)
+  })
+  return response.body
+}
 
 function App() {
+  useEffect(async () => {
+    register("123", "456")
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const mongoose = require('mongoose')
 const { Schema } = mongoose
